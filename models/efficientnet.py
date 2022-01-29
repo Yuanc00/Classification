8487d825e887a973fee93aead51aa10e567e84d4
Yuanc00 <455698977@qq.com>
2022-01-29 22:27:11 +0800

    modify train

---
+++ models/efficientnet.py
@@ -0,0 +1,16 @@
+from torch import nn
+
+
+class Conv3x3(nn.Module):
+    def __init__(self):
+        pass
+
+
+class Fused_MBConv(nn.Module):
+    def __init__(self):
+        pass
+
+
+class MBConv(nn.Module):
+    def __int__(self):
+        pass
