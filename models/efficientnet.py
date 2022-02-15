b2238b2d5afb311c722e970760aa816c07674591
Yuanc00 <455698977@qq.com>
2022-02-15 22:11:31 +0800

    modify train

--- models/efficientnet.py
+++ models/efficientnet.py
@@ -2,8 +2,14 @@ from torch import nn
 
 
 class Conv3x3(nn.Module):
-    def __init__(self):
-        pass
+    def __init__(self, ch_in, ch_out, k=1, s=1):
+        super(Conv3x3, self).__init__()
+        self.conv = nn.Conv2d(ch_in, ch_out, bias=False)
+        self.bn = nn.BatchNorm2d(ch_out)
+        self.act = nn.SiLU()
+
+    def forwark(self, x):
+        return self.act(self.bn(self.conv(x)))
 
 
 class Fused_MBConv(nn.Module):
